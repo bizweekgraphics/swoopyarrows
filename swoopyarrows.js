f4e48edb17743aff63c97e7906b9d863dac1750f
Toph Tucker <tophtucker@gmail.com>
2014-06-12 19:11:30 -0400

    Starting with the version that's been hangin out in bbwboilerplate

---
+++ swoopyarrows.js
@@ -0,0 +1,107 @@
+function drawArrow(parent, from, to, degrees, clockwise) {
+  /*
+  PARAMETERS:
+    parent:     the svg container or element to which to append the arrow
+    from, to:   where to draw the arrow from and to, in any of four forms (in any mix):
+                  a DOM element:            document.getElementById("hed")
+                  a jQuery element:         $("#hed")
+                  a D3 element:             d3.select("#hed")
+                  a coordinate array [x,y]: [100,200]
+    degrees:    the angle which the arc of the arrow will subtend.
+                  90 for a gentle arc, 180 for a bigger swoop.
+                  beyond 180, it gets gentler again, because of the way SVG computes arc.
+                  pass 0 or 360 for a straight arrow.
+    clockwise:  boolean determining whether arrow will swoop clockwise (true) or counterclockwise (false)
+  */
+
+  // ZEROTH, figure out which points to draw between, for when from and to are spatially-extended elements
+
+  // "corners" are coordinates of points that are eligible to be connected
+  function getCorners(element) {
+    if(element instanceof Array && !element.data) {
+      //an array hopefully containing [x,y] was passed in
+      return [{"x":element[0],"y":element[1]}];
+    } else if(element.jquery) {
+      //a jquery element was passed in; convert to DOM element
+      return edgesToCorners(element[0]);
+    } else if(element.nodeType) {
+      //a DOM element was directly passed in
+      return edgesToCorners(element);
+    } else {
+      //assume it's a D3 element (sloppy, yes)
+      return edgesToCorners(element[0][0]);
+    }
+  }
+
+  // gets from the sides of a bounding rect (left, right, top, bottom)
+  //      to its corners (topleft, topright, bottomleft, bottomright)
+  function edgesToCorners(element) {
+    var corners = [];
+    ["left","right"].forEach(function(i) { ["top","bottom"].forEach(function(j) { corners.push({"x":i,"y":j}); }); });
+    return corners.map(function(corner) {
+      return {"x":element.getBoundingClientRect()[corner.x],
+              "y":element.getBoundingClientRect()[corner.y]};
+    });
+  }
+
+  var fromCorners = getCorners(from),
+      toCorners = getCorners(to),
+      fromClosest, toClosest, d;
+  
+  // this seems good to have
+  function distance(from, to) {
+    return Math.sqrt(Math.pow(to.x-from.x,2)+Math.pow(to.y-from.y,2));
+  }
+  
+  // check all possible combinations of eligible endpoints for the shortest distance
+  fromCorners.forEach(function(fromVal) {
+    toCorners.forEach(function(toVal) {
+      if(d==null || distance(fromVal,toVal)<d) {
+        d = distance(fromVal,toVal);
+        fromClosest = fromVal;
+        toClosest = toVal;
+      }
+    });
+  });
+
+  from = fromClosest;
+  to = toClosest;
+
+  /*
+  FIRST, compute radius of circle from desired degrees for arc to subtend.
+    read up:  http://mathworld.wolfram.com/Chord.html
+          http://www.wolframalpha.com/input/?i=angle+subtended
+    n.b.:  bizweek only uses circular arcs, but SVG allows for any ellipse, so r1 == r2 in SVG path below
+        bizweek arrows typically subtend 90 or 180 degrees
+  */
+
+  // bound acceptable {degrees}, between 1 and 359
+  degrees = Math.max(degrees, 1);
+  degrees = Math.min(degrees, 359);
+
+  // get the chord length ("height" {h}) between points, by pythagorus
+  var h = Math.sqrt(Math.pow((to.x-from.x),2)+Math.pow((to.y-from.y),2));
+
+  // get the distance at which chord of height h subtends {angle} degrees
+  var radians = degrees * Math.PI/180;
+  var d = h / ( 2 * Math.tan(radians/2) );
+
+  // get the radius {r} of the circumscribed circle
+  var r = Math.sqrt(Math.pow(d,2)+Math.pow((h/2),2));
+
+  /*
+  SECOND, compose the corresponding SVG arc.
+    read up: http://www.w3.org/TR/SVG/paths.html#PathDataEllipticalArcCommands
+    example: <path d = "M 200 50 a 90 90 0 0 1 100 0"/>
+  */
+  var path = "M " + from.x + " " + from.y + " a " + r + " " + r + " 0 0 "+(clockwise ? "1" : "0")+" " + (to.x-from.x) + " " + (to.y-from.y);
+
+  // append path to given {parent} (with class .arrow)
+  var arrow = parent.append("path")
+    .attr("d", path)
+    .attr("marker-end", "url(#arrowhead)")
+    .attr("class", "arrow");
+
+  // return a reference to the appended arrow
+  return arrow;
+}
